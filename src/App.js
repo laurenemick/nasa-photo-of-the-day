d99ac09bb216900ed029149c453f3ca2405bafb9
Lauren Emick <laurenemick6@gmail.com>
2020-06-10 13:33:40 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+import axios from 'axios'
+
 function App() {
+  const [nasaPhoto, setNasaPhoto] = useState('')
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        //console.log(res.data)
+        setNasaPhoto(res.data.url)
+      })
+      .catch(err => {
+        debugger
+      })
+  }, []) 
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>NASA photo of the day</h1>
+      <img src={nasaPhoto} alt='Photo of the day' />
     </div>
   );
 }
