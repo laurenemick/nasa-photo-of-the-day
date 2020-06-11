5e0d404577c839477e744781c95f2153cd8252d0
Lauren Emick <laurenemick6@gmail.com>
2020-06-11 07:34:07 -0700

    added secrets file for api key and Photo component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import axios from 'axios'
+import { API_KEY } from './secrets'
+
+import Photo from './Photo'
 
 function App() {
   const [nasaPhoto, setNasaPhoto] = useState('')
 
+  /*const [nasaPhotoCollection, setNasaPhotoCollection] = useState([])*/
+
   useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
       .then(res => {
-        //console.log(res.data)
-        setNasaPhoto(res.data.url)
+        console.log(res.data)
+        setNasaPhoto(res.data)
       })
       .catch(err => {
         debugger
       })
   }, []) 
 
+  /*useEffect(() => {
+    axios.get(`https://images-api.nasa.gov?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res.data)
+        setNasaPhotoCollection(res.data)
+      })
+      .catch(err => {
+        debugger
+      })
+  }, []) */
+
   return (
     <div className="App">
       <h1>NASA photo of the day</h1>
-      <img src={nasaPhoto} alt='Photo of the day' />
+      {
+        <Photo nasaPhoto={nasaPhoto} />
+      }
     </div>
   );
 }
