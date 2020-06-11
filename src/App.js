532b284200321f9b7ff7d535884b63c913b4d41e
Lauren Emick <laurenemick6@gmail.com>
2020-06-11 12:24:39 -0700

    added more data

--- src/App.js
+++ src/App.js
@@ -12,11 +12,14 @@ const StyledH1 = styled.h1`
   text-align: center;
   color: blue;
 `;
+const StyledDiv = styled.div`
+  display: flex;
+`;
 
 function App() {
   const [nasaPhoto, setNasaPhoto] = useState('')
 
-  /*const [nasaPhotoCollection, setNasaPhotoCollection] = useState([])*/
+  const [nasaPhotoCollection, setNasaPhotoCollection] = useState([])
 
   useEffect(() => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
@@ -29,23 +32,41 @@ function App() {
       })
   }, []) 
 
-  /*useEffect(() => {
-    axios.get(`https://images-api.nasa.gov?api_key=${API_KEY}`)
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${API_KEY}`)
       .then(res => {
-        console.log(res.data)
-        setNasaPhotoCollection(res.data)
+        console.log(res.data.photos)
+        setNasaPhotoCollection(res.data.photos)
       })
       .catch(err => {
         debugger
       })
-  }, []) */
+  }, []) 
 
+  const Image = ({ info }) => (
+    <div className='marsImages'>
+      <img key={info.id} src={info.img_src} alt={info.id} />
+    </div>
+    )
+    
   return (
     <div className="App">
-      <StyledH1>NASA photo of the day</StyledH1>
-      {
-        <Photo nasaPhoto={nasaPhoto} />
-      }
+      <div>
+        <StyledH1>NASA photo of the day</StyledH1>
+        {
+          <Photo nasaPhoto={nasaPhoto} />
+        }
+      </div>
+      <div className="Container">
+        <StyledH1>Mars Image Collection</StyledH1>
+        <div className="MarsPhotos">
+          {
+            nasaPhotoCollection.map(img => {
+              return <Image key={img.id} info={img} />
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
